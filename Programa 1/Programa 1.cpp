a5bf417980f47f5fcac42ef340fe6fc8011c15d1
Eliseo-Tarqui <goenyi2017@gmail.com>
2025-04-16 11:11:41 -0400

    Programa 1

---
+++ Programa 1/Programa 1.cpp
@@ -0,0 +1,20 @@
+// programa 1.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Leer primer numero" << endl;
+	cin >> a;
+	cout << "Leer segundo numero" << endl;
+	cin >> b;
+	cout << "Leer tercer numero" << endl;
+	cin >> c;
+	
+	int suma = (a + b + c) /3;
+	cout << suma;
+	
+	return 0;
+}
